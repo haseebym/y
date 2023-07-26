9aa3e25bef8087cffc8e84fe4ea273947ebef24f
haseebym <128934502+haseebym@users.noreply.github.com>
2023-07-26 03:15:08 +0300

    Add files via upload
    mm

---
+++ website/language-switcher.js
@@ -0,0 +1,7 @@
+<!-- تحميل ملفات CSS و HTML الأخرى -->
+
+<!-- تضمين ملف JavaScript الخاص بتغيير لغة الموقع -->
+<
+script src = "path/to/language-switcher.js" > < /script> <
+    /body> <
+    /html>
